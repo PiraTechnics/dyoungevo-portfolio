24be35f53b2d13b0b9639a3f68076e06ad32f8c7
PiraTechnics <devin1younge@gmail.com>
2026-09-02 12:31:57 -0700

    Studio section icons and details

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -1,14 +1,53 @@
+import {
+  AdjustmentsVerticalIcon,
+  MicrophoneIcon,
+  MusicalNoteIcon,
+  SpeakerWaveIcon,
+} from "@heroicons/react/20/solid";
+
+const studioItems = [
+  {
+    name: "Microphone",
+    details: "Blue Ember Condenser XLR",
+    icon: MicrophoneIcon,
+  },
+  {
+    name: "Interface",
+    details: "Scarlett 2i2 (3rd Generation)",
+    icon: AdjustmentsVerticalIcon,
+  },
+  {
+    name: "DAW",
+    details: "Logic Pro",
+    icon: MusicalNoteIcon,
+  },
+  {
+    name: "Headphones",
+    details: "Sony MDR-7506",
+    icon: SpeakerWaveIcon,
+  },
+];
+
 export default function Studio() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-semibold text-pretty tracking-tight sm:text-4x  text-gray-400 dark:text-gray-300">
-        Studio
+      <h2 className="text-3xl font-semibold text-pretty tracking-tight text-gray-400 sm:text-4xl lg:text-5xl dark:text-gray-300">
+        Home Studio
       </h2>
-      <ul className="px-12 py-4 text-gray-400 dark:text-gray-300">
-        <li>Microphone: Blue Ember Condenser XLR</li>
-        <li>DAW: Logic Pro</li>
-        <li>Interface: Scarlet 2i2 (3rd Generation)</li>
-      </ul>
+      <dl className="mt-10 space-y-8 sm:text-lg lg:text-xl text-gray-600 dark:text-gray-400">
+        {studioItems.map((item) => (
+          <div key={item.name} className="relative pl-9">
+            <dt className="inline font-semibold text-gray-900 dark:text-white">
+              <item.icon
+                aria-hidden="true"
+                className="absolute top-1 left-1 size-5 text-indigo-600 dark:text-indigo-400"
+              />
+              {item.name}
+            </dt>{" "}
+            <dd className="">{item.details}</dd>
+          </div>
+        ))}
+      </dl>
     </div>
   );
 }
