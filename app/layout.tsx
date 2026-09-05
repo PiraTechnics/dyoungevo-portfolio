85faca836f543bed89cde904aa9c30083ce1073e
PiraTechnics <devin1younge@gmail.com>
2026-09-05 15:08:57 -0700

    Studio mic emoji as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${playfairDisplay.variable} h-full antialiased bg-black`}
     >
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎙️</text></svg>"
+      ></link>
       <body className="min-h-full bg-linear-to-b from-indigo-950 via-indigo-900 to-blue-900 pb-24">
         <Header />
         <div className="px-6 pt-8 sm:pt-16 lg:px-8">{children}</div>
