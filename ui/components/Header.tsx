094d6b9c44dbad8571b19091b999bf3c507852fd
PiraTechnics <devin1younge@gmail.com>
2026-09-06 09:49:24 -0700

    hand-drawn signature

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -1,14 +1,14 @@
 import { MobileMenu } from "./MobileMenu";
 import { NavLinks } from "./NavLinks";
 import Image from "next/image";
-import SignatureSVG from "@/public/signaturePlaceholder.svg";
+import Signature from "@/public/signature1.png";
 
 export const Header = () => {
   return (
     <header className="top-0 z-10 w-full h-fit py-1 text-white bg-slate-900/70">
       <nav className="flex px-8 justify-between h-full mx-auto">
         <Image
-          src={SignatureSVG}
+          src={Signature}
           alt={"Devin Younge"}
           height={50}
           priority
