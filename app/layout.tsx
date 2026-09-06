94de134da54cdc9bb7d1d3a7d5c41fc335373ee8
PiraTechnics <devin1younge@gmail.com>
2026-09-05 17:29:55 -0700

    Vercel Analytics enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/ui/components/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full bg-linear-to-b from-indigo-950 via-indigo-900 to-blue-900 pb-24">
         <Header />
         <div className="px-6 pt-8 sm:pt-16 lg:px-8">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
