abafa8948e6d3d82c78a585703265bd5ec19aaad
PiraTechnics <devin1younge@gmail.com>
2026-09-06 10:10:13 -0700

    Improved artist signature

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -1,12 +1,12 @@
 import { MobileMenu } from "./MobileMenu";
 import { NavLinks } from "./NavLinks";
 import Image from "next/image";
-import Signature from "@/public/signature1.png";
+import Signature from "@/public/signature2.png";
 
 export const Header = () => {
   return (
     <header className="top-0 z-10 w-full h-fit py-1 text-white bg-slate-900/70">
-      <nav className="flex px-8 justify-between h-full mx-auto">
+      <nav className="flex pe-8 justify-between h-full mx-auto">
         <Image
           src={Signature}
           alt={"Devin Younge"}
